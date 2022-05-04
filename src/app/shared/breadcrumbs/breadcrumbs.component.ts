10a317a9793ef8ff95a0c941df0500ad2b03817d
Gilmer Contreras <adgdiux@gmail.com>
2022-05-04 18:24:17 -0500

    Breadcrumbs - Routing - Params Router

--- src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivationEnd, Router } from '@angular/router';
+import { filter, map } from 'rxjs';
 
 @Component({
   selector: 'app-breadcrumbs',
@@ -8,7 +10,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BreadcrumbsComponent implements OnInit {
 
-  constructor() { }
+  public page!: string;
+
+  constructor(  private router: Router) { 
+    
+    this.router.events
+        .pipe(
+          filter( event => event instanceof ActivationEnd ),
+          filter( (event: any) => event.snapshot.firstChild === null  ),
+          map( (event: any) => event.snapshot.data )
+        )
+        .subscribe( data => {
+
+          this.page = data.title;
+          document.title = `${data.title} - LTC System`;
+        
+        }); 
+  }
 
   ngOnInit(): void {
   }

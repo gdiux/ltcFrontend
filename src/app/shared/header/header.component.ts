8edd0d70303c1d6ff542d9fd0c6279e65f94b23c
Gilmer Contreras <adgdiux@gmail.com>
2022-06-09 02:04:26 -0500

    Buscador de productos en el header

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -5,6 +5,8 @@ import { User } from 'src/app/models/users.model';
 
 // SERVICE
 import { UsersService } from '../../services/users.service';
+import { SearchService } from 'src/app/services/search.service';
+import { Product } from 'src/app/models/products.model';
 
 @Component({
   selector: 'app-header',
@@ -16,7 +18,8 @@ export class HeaderComponent implements OnInit {
 
   public user!: User;
 
-  constructor(  private usersService: UsersService) { 
+  constructor(  private usersService: UsersService,
+                private searchService: SearchService) { 
     
     // CARGAR USER
     this.user = usersService.user;
@@ -26,6 +29,37 @@ export class HeaderComponent implements OnInit {
   ngOnInit(): void {
 
   }
+
+  /** ==============================================================================
+   * BUSCAR PRODUCTOS
+  ================================================================================*/
+  public resultados: number = 0;
+  public products: Product[] = [];
+  search( termino:string ){
+
+    let query = `desde=0&hasta=1000`;
+
+    if (termino.length === 0) {
+      this.products = [];
+      this.resultados = 0;
+      return;
+    }
+    
+    this.searchService.search('products', termino, query)
+        .subscribe( ({resultados}) => {
+
+          this.products = resultados;
+          this.resultados = resultados.length;
+
+        });   
+
+  }
+
+
+
+  /** ==============================================================================
+   * LOGOUT
+  ================================================================================*/
 
   logout(){
     this.usersService.logout();
